aa378e64d80d0cc56aa1b722da8ff8377afa8dae
Nihad Mammadli <nihadmammadli03@gmail.com>
2026-01-05 00:11:38 +0400

    Change player shape from circle to triangle; update movement rotation logic and instructions

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -13,7 +13,7 @@ function Game() {
     const cellSize = 50
 
     class GameScene extends Phaser.Scene {
-      private player!: Phaser.GameObjects.Arc
+      private player!: Phaser.GameObjects.Triangle
       private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
       private gridX = 0
       private gridY = 0
@@ -23,7 +23,6 @@ function Game() {
       }
 
       create() {
-        // Draw 8x8 grid
         const graphics = this.add.graphics()
         graphics.lineStyle(2, 0x00ff00)
         for (let i = 0; i <= 16; i++) {
@@ -31,22 +30,47 @@ function Game() {
           graphics.lineBetween(0, i * cellSize, 800, i * cellSize)
         }
 
-        // Create player (circle)
-        this.player = this.add.circle(cellSize / 2, cellSize / 2, 20, 0xff0000)
+        // Create player (isosceles triangle pointing right, centered)
+        this.player = this.add.triangle(
+          cellSize / 2,
+          cellSize / 2,
+
+          // points adjusted so centroid is at (0,0)
+          0, -10,     // top
+          12, 10,     // bottom right
+          -12, 10,    // bottom left
+
+          0xff0000
+        )
 
         // Setup keyboard input
         this.cursors = this.input.keyboard!.createCursorKeys()
       }
 
       update() {
+        let moved = false
+        let newRotation = this.player.rotation
+
         if (Phaser.Input.Keyboard.JustDown(this.cursors.left) && this.gridX > 0) {
           this.gridX--
+          newRotation = Math.PI // 180 degrees - pointing left
+          moved = true
         } else if (Phaser.Input.Keyboard.JustDown(this.cursors.right) && this.gridX < 15) {
           this.gridX++
+          newRotation = 0 // 0 degrees - pointing right
+          moved = true
         } else if (Phaser.Input.Keyboard.JustDown(this.cursors.up) && this.gridY > 0) {
           this.gridY--
+          newRotation = -Math.PI / 2 // -90 degrees - pointing up
+          moved = true
         } else if (Phaser.Input.Keyboard.JustDown(this.cursors.down) && this.gridY < 15) {
           this.gridY++
+          newRotation = Math.PI / 2 // 90 degrees - pointing down
+          moved = true
+        }
+
+        if (moved) {
+          this.player.rotation = newRotation
         }
 
         // Update player position
@@ -74,7 +98,7 @@ function Game() {
 
   return (
     <div className={styles.container}>
-      <p className={styles.instructions}>Use arrow keys to move the red circle</p>
+      <p className={styles.instructions}>Use arrow keys to move the red triangle</p>
       <div ref={gameRef} className={styles.gameContainer} />
       <Link to="/" className={styles.backLink}>
         Back to Home
